c25ca89a32ac9b30b16bd81ba0bca530ef838aa0
NeverSeen <adrian115185@gmail.com>
2020-02-03 01:56:29 +0100

    CrsMuOnline 2020

---
+++ crsmu/js/main.js
@@ -0,0 +1,32 @@
+function cos(){
+
+    var data = new Date();
+    var test;
+    var test2;
+    var test3;
+    var godzina = data.getHours();
+      if(godzina<10){
+        test3 = "0" + godzina;
+      }
+      else{
+        test3 = godzina;
+      }
+    var minuta = data.getMinutes();
+    if(minuta<10){
+      test2 = "0" + minuta;
+    }
+    else{
+      test2 = minuta;
+    }
+    
+    var sekunda = data.getSeconds();
+    if(sekunda<10){
+      test = "0" + sekunda;
+    }
+    else{
+      test = sekunda;
+    }
+    document.getElementById("clock").innerHTML = "CZAS SERWERA: " + test3 + ":" + test2 + ":" + test;
+      
+      setTimeout(cos, 1000);
+    }
