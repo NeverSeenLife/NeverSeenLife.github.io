0fa35d91d12ab2de64ae75c21ca97b5fc2c47d61
NeverSeen <adrian115185@gmail.com>
2020-06-28 01:04:22 +0200

    update joinery

--- tpjoinery/js/plugins.js
+++ tpjoinery/js/plugins.js
@@ -32,3 +32,32 @@ $(function() {
   });
 });
 
+$(document).scroll(function () {
+  var y = $(this).scrollTop();
+  if (y > 300) {
+      $('.page_up').fadeIn();
+  } else {
+      $('.page_up').fadeOut();
+  }
+
+});
+
+$('img[data-enlargable]').addClass('img-enlargable').click(function(){
+  var src = $(this).attr('src');
+  var modal;
+  function removeModal(){ modal.remove(); $('body').off('keyup.modal-close'); }
+  modal = $('<div>').css({
+      background: 'RGBA(0,0,0,.5) url('+src+') no-repeat center',
+      backgroundSize: 'contain',
+      width:'100%', height:'100%',
+      position:'fixed',
+      zIndex:'10000',
+      top:'0', left:'0',
+      cursor: 'pointer'
+  }).click(function(){
+      removeModal();
+  }).appendTo('body');
+  $('body').on('keyup.modal-close', function(e){
+    if(e.key==='Escape'){ removeModal(); } 
+  });
+});
