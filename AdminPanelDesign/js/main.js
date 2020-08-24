0af33d14ce6f195ae3dbc2a3ac9c7183985b0ec9
NeverSeen <adrian115185@gmail.com>
2020-08-24 02:25:16 +0200

    update

--- AdminPanelDesign/js/main.js
+++ AdminPanelDesign/js/main.js
@@ -1,20 +1,29 @@
-const rmCheck = document.getElementById("rememberPass"),
-    emailInput = document.getElementById("pass");
-
-if (localStorage.checkbox && localStorage.checkbox !== "") {
-  rmCheck.setAttribute("checked", "checked");
-  emailInput.value = localStorage.username;
-} else {
-  rmCheck.removeAttribute("checked");
-  emailInput.value = "";
-}
-
-function lsRememberMe() {
-  if (rmCheck.checked && emailInput.value !== "") {
-    localStorage.username = emailInput.value;
-    localStorage.checkbox = rmCheck.value;
-  } else {
-    localStorage.username = "";
-    localStorage.checkbox = "";
+function zegarek(){
+  var data = new Date();
+  var test;
+  var test2;
+  var test3;
+  var godzina = data.getHours();
+    if(godzina<10){
+      test3 = "0" + godzina;
+    }
+    else{
+      test3 = godzina;
+    }
+  var minuta = data.getMinutes();
+  if(minuta<10){
+    test2 = "0" + minuta;
   }
-}
+  else{
+    test2 = minuta;
+  }
+  var sekunda = data.getSeconds();
+  if(sekunda<10){
+    test = "0" + sekunda;
+  }
+  else{
+    test = sekunda;
+  }
+  document.querySelector(".timer").innerHTML = test3 + ":" + test2 + ":" + test;
+    setTimeout(zegarek, 1000);
+  }window.onload = zegarek();
