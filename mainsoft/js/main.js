128949985bb2e4ed798103a44e8ba6a73b657216
NeverSeen <adrian115185@gmail.com>
2020-10-04 03:58:10 +0200

    mainsoft pro

--- mainsoft/js/main.js
+++ mainsoft/js/main.js
@@ -5,11 +5,11 @@ let isHamburherOpen = false;
 hamburger.addEventListener('click', () =>{
     if(!isHamburherOpen){
         hamburger.classList.add('active');
-        menu.classList.add('open');
+
         isHamburherOpen = true;
     }else{
         hamburger.classList.remove('active');
-        menu.classList.remove('open');
+
         isHamburherOpen = false;
     }
 });
@@ -19,3 +19,22 @@ const logo = document.querySelector('.logoImg');
 logo.addEventListener('click', () =>{
     window.scrollTo(0,document.body.scrollHeight);
 });
+
+window.onscroll = function() {scrollMenu()};
+
+function scrollMenu(){
+    var header = document.querySelector(".headerBar"), logo = document.querySelector(".logoImg"), navBtn = document.querySelectorAll(".menuBtn");
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        header.classList.add("headerBarScrolled");
+        logo.classList.add("logoImgScrolled");
+        navBtn.forEach(item => {
+            item.classList.add("nav--BtnScrolled");
+        });
+      } else {
+        header.classList.remove("headerBarScrolled");
+        logo.classList.remove("logoImgScrolled");
+        navBtn.forEach(item => {
+            item.classList.remove("nav--BtnScrolled");
+        });
+      }
+    }
