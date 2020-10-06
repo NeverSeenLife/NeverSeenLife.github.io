6279b0021adec0fdcf6d41b6fc78f43c7d7a6c0f
NeverSeen <adrian115185@gmail.com>
2020-10-06 18:42:14 +0200

    es

--- mainsoft/js/main.js
+++ mainsoft/js/main.js
@@ -14,6 +14,25 @@ hamburger.addEventListener('click', () =>{
     }
 });
 
+var more = document.querySelector('.more');
+var btn = document.querySelector('.clickMore');
+let isMoreOpen = false; 
+
+btn.addEventListener('click', () =>{
+    if(!isMoreOpen){
+        more.classList.add('active');
+        isMoreOpen = true;
+    }
+    else{
+        more.classList.remove('active');
+        isMoreOpen = false;
+    }
+});
+document.querySelector('.closeBtn').addEventListener('click', () => {
+    more.classList.remove('active');
+})
+
+
 window.onscroll = function() {
     scrollMenu();
     scrollArrow();
