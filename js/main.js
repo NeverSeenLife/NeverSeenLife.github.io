6caf1f5c22fe85c9a72d373fc848094751c59810
NeverSeen <akaja@mainsoft.pl>
2021-03-27 03:00:30 +0100

    jesienny update

--- js/main.js
+++ js/main.js
@@ -60,3 +60,4 @@ function scrollSmoothTo(elementId) {
     var element = document.getElementById(elementId);
     element.scrollIntoView({ block: 'start',  behavior: 'smooth' });
 }
+setTimeout(function(){ console.clear();console.log("%cHi! NeverSeen was here", "color: blue; font-size: x-large");}, 5000);
